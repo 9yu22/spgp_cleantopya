594300299e7de227975e2ce4a83335d6044387fe
9yu22 <ehtlfk2@naver.com>
2024-04-11 15:32:58 +0900

    text, button 정렬. (GameActivity에 GameView를 넣고싶다.)
    문제점
    * 흔하게 애뮬레이터를 실행한 후 버튼을 누르면 android studio 자체만 멈추는것이 아닌, 노트북 자체가 동작없음. 노트북 전원버튼을 아예 오랫동안 눌러 꺼서 재시작하는 방법밖에 없음..
    
    - 창 간 전환시, 너무 느려서 전환이 안되는 줄 알고있었음.
    - 창 간 전환시, 애뮬레이터 창을 늘이거나 줄이는 등, 다른 창에 갔다가 다시 오는 등 변화가 생겨야 전환이 되는문제 -> android studio를 다시 시작하면 제대로 동작하기도함. 혹은 <재시작>의 문제인가?
    
    -계속 View를 새로 생성해서 넣으려는데 문제가 발생하는것으로 보임. View를 넣은 Activity로 전환하는 버튼을 누르면 앱이 중지됨.

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -6,11 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
     Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +20,10 @@ public class GameActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // GameActivity를 닫고 MainActivity를 시작
                 Intent intent = new Intent(GameActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish(); // 현재 액티비티 종료
             }
         });
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("MainActivity", "Main Button clicked!"); // 클릭이벤트 발생 로그 출력
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
             }
