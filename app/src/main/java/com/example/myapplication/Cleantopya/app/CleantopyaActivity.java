dcdb6886b43760a7ee6d5d32b108e821e84aee60
9yu22 <ehtlfk2@naver.com>
2024-05-09 16:37:07 +0900

    VideoView를 상속받는 카운트다운 비디오 구현시도

--- app/src/main/java/com/example/myapplication/Cleantopya/app/CleantopyaActivity.java
+++ app/src/main/java/com/example/myapplication/Cleantopya/app/CleantopyaActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication.Cleantopya.app;
 import android.os.Bundle;
 
 import com.example.myapplication.BuildConfig;
+import com.example.myapplication.Cleantopya.game.StartVideo;
 import com.example.myapplication.Cleantopya.game.stage1Scene;
 import com.example.myapplication.framework.activity.GameActivity;
 import com.example.myapplication.framework.view.Metrics;
@@ -52,13 +53,14 @@ public class CleantopyaActivity extends GameActivity {
         // BroadcastReceiver 해제
         unregisterReceiver(customReceiver);
     }*/
-
     protected void onCreate(Bundle savedInstanceState) {
         Scene.drawsDebugInfo = BuildConfig.DEBUG;
         Metrics.setGameSize(9, 16);
         super.onCreate(savedInstanceState);
         // Scene.drawsDebugInfo 변경 시점에 주의한다.
         // new GameView() 가 호출되는 super.onCreate() 보다 이전에 해야 한다.
+        StartVideo video = new StartVideo(this);
+        video.start();
         new stage1Scene().push();
     }
 
