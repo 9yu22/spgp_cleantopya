07973eb4a9a23835a8bf63652894af7c892bb3f5
9yu22 <ehtlfk2@naver.com>
2024-06-01 15:24:08 +0900

    카운트다운 영상 추가

--- app/src/main/java/com/example/myapplication/Cleantopya/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Cleantopya/app/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.myapplication.Cleantopya.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.example.myapplication.R;
 
@@ -18,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button_start);
+
+        VideoView videoview1 = findViewById(R.id.startVideo);
+
+        String uriPath2 = "android.resource://com.example.myapplication/"+R.raw.countdown; //흠
+        Uri uri2 = Uri.parse(uriPath2);
+        videoview1.setVideoURI(uri2);
+        videoview1.requestFocus();
+        videoview1.start();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
