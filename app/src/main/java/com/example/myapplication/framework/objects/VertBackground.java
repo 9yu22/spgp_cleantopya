96c8d8f8e41fa767280c80ef4aba5a6e7ecd90ff
9yu22 <ehtlfk2@naver.com>
2024-06-07 15:34:32 +0900

    로그정리, background클래스 rename, stage1score 저장, yellowButton버그 수정(보이지않아도 눌림)

--- app/src/main/java/com/example/myapplication/framework/objects/VertBackground.java
+++ app/src/main/java/com/example/myapplication/framework/objects/VertBackground.java
@@ -5,18 +5,16 @@ import android.graphics.Canvas;
 import com.example.myapplication.framework.view.Metrics;
 
 public class VertScrollBackground extends Sprite {
-    private final float speed;
+
     //private final float height;
-    public VertScrollBackground(int bitmapResId, float speed) {
+    public VertScrollBackground(int bitmapResId) {
         super(bitmapResId);
         //this.height = bitmap.getHeight() * Metrics.width / bitmap.getWidth();
         //setPosition(Metrics.width / 2, Metrics.height / 2, Metrics.width, height);
         setPosition(0,0, Metrics.width, Metrics.height);
-        this.speed = speed;
     }
     @Override
     public void update(float elapsedSeconds) {
-        this.y += speed * elapsedSeconds; // y 값을 스크롤된 양으로 사용한다
     }
 
     @Override
@@ -25,7 +23,7 @@ public class VertScrollBackground extends Sprite {
         float curr = y % height;
         if (curr > 0) curr -= height;
         while (curr < Metrics.height) {
-            dstRect.set(0, curr, Metrics.width, curr + height);
+            dstRect.set(0, 0, Metrics.width, height);
             canvas.drawBitmap(bitmap, null, dstRect, null);
             curr += height;
         }
